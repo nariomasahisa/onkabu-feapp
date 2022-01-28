c4101514b42e42d2bce89def3f4d5f91ed49be2d
nariomasahisa <nariomasahisa@gmail.com>
2022-01-28 09:30:33 +0900

    Revert "docker削除"
    This reverts commit 69a2688c976d7cdba450960fbd862a692ed693d5.

--- src/app/calc-value/calc-value.component.ts
+++ src/app/calc-value/calc-value.component.ts
@@ -10,59 +10,23 @@ import { Value } from './voes/value';
   styleUrls: ['./calc-value.component.scss']
 })
 export class CalcValueComponent {
-  @Input('title') titleName: string = '企業価値の計算';
-
-  public isInputFlg: boolean = true;
+  @Input('title') titleName: string = '企業価値の計算'
 
   public stockCode = new FormControl('');
   public stockName = new FormControl('');
   public ownerIncome = new FormControl('');
   public roic = new FormControl('');
-  public values: Value[] = [];
 
-  private file: File | null = null;
+  public values: Value[] = [];
 
   constructor(private router: Router, private calcService: CalcService) { }
 
-  /**
-   * input画面での企業価値を計算するメソッド
-   * @returns 企業価値を返す(数値型)
-   */
   public calcStockValue() {
     this.values = [];
-    // calcServiceへ値を渡す
     this.calcService.inputData(this.roic.value / 100, this.ownerIncome.value)
     this.values = this.calcService.values
     return this.values;
   }
 
-  /**
-   * テンプレートのファイルアップ画面と入力画面の切り替えを司るメソッド
-   */
-  public changeView():void {
-    this.isInputFlg = !this.isInputFlg;
-  }
-
-  /**
-   * 
-   * @param event ファイルアップロード時に発動
-   * @returns 
-   */
-  public importFile(event: Event): void {
-    
-    const files: FileList = (event.target as any).files;
-    if (files.length === 0) {
-      return
-    }
-  }
-
-  
-  public get filename() : string {
-    if (this.file !== null) {
-      return this.file.name
-    }
-    return '';
-  }
-  
-
 }
+``
