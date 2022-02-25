d79ca9df81936939a8c496f3b63376e3614ba617
nariomasahisa <nariomasahisa@gmail.com>
2022-02-25 20:30:43 +0900

    urlの修正
    .envに入れる必要がある

--- src/api/onkabu/onkabu-web.service.ts
+++ src/api/onkabu/onkabu-web.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { environment } from 'src/environments/environment';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +13,8 @@ export class OnkabuWebService {
   ) { }
 
   public calcOnkabu(buy:number, now:number, stocks:number):Promise<any>{
-    // const path = `${environment.apiUrl}/dev/onkabu`;
-    const prodPath = `${environment.apiUrl}/dev/onkabu`
-    return this.http.post<any>(prodPath, {buy, now, stocks}).toPromise();
+    const path = `${environment.apiUrl}/dev/onkabu`;
+    // const prodPath = `${environment.apiUrl}/dev/onkabu`
+    return this.http.post<any>(path, {buy, now, stocks}).toPromise();
   }
 }
