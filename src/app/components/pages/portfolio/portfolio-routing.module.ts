71c87fac094fdabaae86a19016a024cc3455432d
nariomasahisa <nariomasahisa@gmail.com>
2022-05-15 00:17:42 +0900

    edit routing module
    add routing about CREATE and EDIT

--- src/app/components/pages/portfolio/portfolio-routing.module.ts
+++ src/app/components/pages/portfolio/portfolio-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioCreateComponent } from './portfolio-create/portfolio-create.component';
+import { PortfolioEditComponent } from './portfolio-edit/portfolio-edit.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PortfolioComponent
+    component: PortfolioComponent,
   },
-  // {
-  //   path: 'create',
-  //   component: 
-  // }
+  {
+    path: 'create',
+    component: PortfolioCreateComponent
+  },
+  {
+    path: 'edit',
+    component: PortfolioEditComponent
+  }
 ]
 
 @NgModule({
