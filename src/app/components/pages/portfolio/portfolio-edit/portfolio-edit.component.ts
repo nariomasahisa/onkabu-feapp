0f21c7081eea2f58fd18f6ca6080c09fa599b360
nariomasahisa <nariomasahisa@gmail.com>
2022-05-15 00:16:01 +0900

    add edit component

---
+++ src/app/components/pages/portfolio/portfolio-edit/portfolio-edit.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-edit',
+  templateUrl: './portfolio-edit.component.html',
+  styleUrls: ['./portfolio-edit.component.scss']
+})
+export class PortfolioEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  save(): void {
+    console.log('saved')
+  }
+}
